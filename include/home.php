b0771166883c6365348bcc218c8ca64711255290
mehrdad-parsa68 <mehrdad.parsa68@gmail.com>
2015-03-12 18:07:13 +0330

    s

--- include/home.php
+++ include/home.php
@@ -204,6 +204,8 @@
 					<ul class="itemwrap">
 						<li class="current"><a class="example-image-link" href="<?php echo $prefix; ?>/images/press/pic01.jpg" data-lightbox="example-set">
                     <img class="example-image img-thumbnail" src="<?php echo $prefix; ?>/images/press/pic01-t.jpg" alt="رای وب"/></a><h4 align="center">روزنامه ابتکار</h4></li>
+                    <li class="current"><a class="example-image-link" href="<?php echo $prefix; ?>/images/press/pic02.jpg" data-lightbox="example-set">
+                    <img class="example-image img-thumbnail" src="<?php echo $prefix; ?>/images/press/pic02-t.jpg" alt="رای وب"/></a><h4 align="center">مجله برین کارت</h4></li>
 					
 					</ul>
 					<nav>
